251ec66dd0dff8ef1f5e64194e175a553af0d8bf
rohitbegani <rbegani72@gmail.com>
2014-11-30 12:35:00 +0530

    Made changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rottenpotatoes::Application.routes.draw do
   #   resources :products
   resources :movies
 
+
+  root :to => redirect('/movies')
   # Sample resource route with options:
   #   resources :products do
   #     member do
